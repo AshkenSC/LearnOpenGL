31d33cf65817ab4102b75878e29f2294d0d61e0d
AshkenSC <393940378@qq.com>
2019-07-23 23:58:50 +0800

    Shader: adjusting shader to change the color of triangle

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ const char *vertexShaderSource = "#version 330 core\n"
 "void main()\n"
 "{\n"
 "	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"vertexColor = vec4(0.5, 0.0, 0.0, 1.0);"
 "}\n";
 
 // fragment shader
