c9adaac478780fa19c5bbb740b7dc5196c31fc2d
AshkenSC <393940378@qq.com>
2019-07-31 23:59:41 +0800

    Shader: adding constructor for Shader class

--- shader.cpp
+++ shader.cpp
@@ -23,6 +23,34 @@ public:
 	void setBool(const std::string &name, bool value) const;
 	void setInt(const std::string &name, int value) const;
 	void setFloat(const std::string &name, float value) const;
+
+	Shader(const char* vertexPath, const char* fragmentPath)
+	{
+		// 1. 从文件路径中获取顶点/片段着色器
+		std::string vertexCode;
+		std::string fragmentCode;
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+		// 保证ifstream对象可以抛出异常：
+		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		try
+		{
+			// 打开文件
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentPath);
+			std::stringstream vShaderStream, fShaderStream;
+			// 读取文件的缓冲内容到数据流中
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderStream << fShaderFile.rdbuf();
+			// 关闭文件处理器
+			vShaderFile.close();
+			fShaderFile.close();
+			// 转换数据流到string
+			vertexCode = vShaderStream.str();
+			fragmentCode = fShaderStream.str();
+		}
 };
 
 #endif
+
