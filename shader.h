68d43374bad4d5cf0d7b6914a19e1e6a8663c948
AshkenSC <393940378@qq.com>
2019-08-29 13:12:42 +0800

    Texture: setMat3 function declaration has been added to shader.h.

--- shader.h
+++ shader.h
@@ -150,6 +150,11 @@ public:
 	{
 		glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 	}
+	// ------------------------------------------------------------------------
+	void setMat3(const std::string &name, const glm::mat3 &mat) const
+	{
+		glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
 
 private:
 	// utility function for checking shader compilation/linking errors.
