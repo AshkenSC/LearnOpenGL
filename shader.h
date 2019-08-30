607e833892a433dc588d23952b50987c24a417ae
AshkenSC <393940378@qq.com>
2019-08-30 23:52:51 +0800

    Texture: setMat4 function declaration has been added to shader.h.

--- shader.h
+++ shader.h
@@ -155,6 +155,11 @@ public:
 	{
 		glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 	}
+	// ------------------------------------------------------------------------
+	void setMat4(const std::string &name, const glm::mat4 &mat) const
+	{
+		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
 
 private:
 	// utility function for checking shader compilation/linking errors.
