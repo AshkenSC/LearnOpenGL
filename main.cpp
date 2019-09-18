5f239e61efe1504ff8b21dc6abe1bcdbd5bfcf1a
AshkenSC <393940378@qq.com>
2019-09-18 22:50:46 +0800

    Transformation: Practice 2 in progress: drawing the first container.

--- main.cpp
+++ main.cpp
@@ -178,16 +178,18 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		// create transformations
 		glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-		// TRANSFORM PRACTICE 1: swap, first rotate then translate
-		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+		// first container
 		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-
+		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 		// get matrix's uniform location and set matrix
-		ourShader.use();
 		unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+		// with the uniform matrix set, draw the first container
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		// TODO: second transformation
 
 		// PRACTICE 3: set a uniform value to modify texture transparency
 		ourShader.setFloat("texTrans", texTrans);
