1a409ab223b3df6498925d9937eb8b857f46ccf2
AshkenSC <393940378@qq.com>
2019-09-25 09:56:13 +0800

    Transformation: Practice 2 in progress: second transformation finished.

--- main.cpp
+++ main.cpp
@@ -189,9 +189,15 @@ int main()
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-		// TODO: second transformation
+		// TRANSFORMATION PRACTICE 2: second transformation
+		// ---------------------
+		transform = glm::mat4(1.0f); // reset it to identity matrix
+		transform = glm::translate(transform, glm::vec3(-0.5f, 0.5f, 0.0f));
+		float scaleAmount = sin(glfwGetTime());
+		transform = glm::scale(transform, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &transform[0][0]); // this time take the matrix value array's first element as its memory pointer value
 
-		// PRACTICE 3: set a uniform value to modify texture transparency
+		// TEXTURE PRACTICE 3: set a uniform value to modify texture transparency
 		ourShader.setFloat("texTrans", texTrans);
 
 		// render container
