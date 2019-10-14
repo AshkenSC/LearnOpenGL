b1f3a17efc1f6324da9299fc09494382daaeaa42
AshkenSC <393940378@qq.com>
2019-10-14 22:26:52 +0800

    Camera: The code of shaders has been cleaned up.

--- shader.fs
+++ shader.fs
@@ -13,10 +13,5 @@ uniform float texTrans;
 
 void main()
 {
-	//FragColor = vec4(ourColor, 1.0);
-
-	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	// add "* vec4(ourColor, 1.0)" behind to make the box colorful
-	// practice 1: reverse the direction of the happy face
 	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), texTrans);
 }
--- shader.vs
+++ shader.vs
@@ -15,7 +15,6 @@ uniform mat4 projection;
 
 void main()
 {
-	//gl_Position = vec4(aPos.x + xOffset, aPos.y, aPos.z, 1.0);
 	gl_Position = projection * view * model *transform * vec4(aPos,  1.0);	// use vec3 as vec4 constructor's parameter
 	ourColor = aColor;		
 	TexCoord = vec2(aTexCoord.x, aTexCoord.y);
