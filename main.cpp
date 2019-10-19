8b8a216eab5f132f1cd87260cf27e247822244f4
AshkenSC <393940378@qq.com>
2019-10-19 14:48:02 +0800

    Camera: Camera movement speed has been synchronized using deltaTime.

--- main.cpp
+++ main.cpp
@@ -31,8 +31,18 @@ glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
 int main()
 {
+	// CAMERA: per-frame time logic
+	// --------------------
+	float currentFrame = glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
+
 	// glfw: initialize and configure
 	// ------------------------------
 	glfwInit();
@@ -335,6 +345,9 @@ int main()
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
+	// take deltaTime into control when calculating speed
+	float cameraSpeed = 2.5f * deltaTime;
+
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
