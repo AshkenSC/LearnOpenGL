64aee8ed7e1634abc58040d55ca121d38181004c
AshkenSC <393940378@qq.com>
2019-11-27 21:30:11 +0800

    Camera: Refactoring code: building Camera class, constructors have been added.

--- Camera.h
+++ Camera.h
@@ -38,6 +38,29 @@ public:
 	float MouseSensitivity;
 	float Zoom;
 
+	// Constructor with vectors
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		Position = position;
+		WorldUp = up;
+		Yaw = yaw;
+		Pitch = pitch;
+		updateCameraVectors();
+	}
+	// Constructor with scalar values
+	Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		Position = glm::vec3(posX, posY, posZ);
+		WorldUp = glm::vec3(upX, upY, upZ);
+		Yaw = yaw;
+		Pitch = pitch;
+		updateCameraVectors();
+	}
 
+private:
+	// Calculates the front vector from the Camera's (updated) Euler Angles
+	void updateCameraVectors() {
+
+	}
 };
 #endif
