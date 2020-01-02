ebccf24192c9aa15655664dbe9a3d7fb872c2d8b
AshkenSC <393940378@qq.com>
2020-01-02 21:53:43 +0800

    Basic Lighting: Fragment shader has been updated. Final result calculation has been fixed.

--- colors.fs
+++ colors.fs
@@ -12,13 +12,13 @@ void main()
 	
 	float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
-
-    vec3 result = ambient * objectColor;
-    FragColor = vec4(result, 1.0);
     
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
 
 	float diff = max(dot(norm, lightDir), 0.0);
-vec3 diffuse = diff * lightColor;
+	vec3 diffuse = diff * lightColor;
+
+	vec3 result = (ambient + diffuse) * objectColor;
+	FragColor = vec4(result, 1.0);
 }
