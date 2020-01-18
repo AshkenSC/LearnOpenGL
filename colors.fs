67289978f759bd8c87fb51ba0fca8aa30eaab192
AshkenSC <393940378@qq.com>
2020-01-18 20:06:49 +0800

    Materials: Material attributes struct has been created in the vertex shader.

--- colors.fs
+++ colors.fs
@@ -1,4 +1,14 @@
 #version 330 core
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
+
+uniform Material material;
+// TODO
+
 out vec4 FragColor;
 
 in vec3 LightingColor; 
