3d63dd157ad84282d170d2b6b7d650574b4cfe88
AshkenSC <393940378@qq.com>
2020-01-19 23:21:05 +0800

    Materials: Fragment shader has been updated to re-calculate material attributes.

--- colors.fs
+++ colors.fs
@@ -5,18 +5,32 @@ struct Material {
     vec3 specular;
     float shininess;
 }; 
-
-uniform Material material;
-// TODO
+// up TODO
 
 out vec4 FragColor;
 
 in vec3 LightingColor; 
 
 uniform vec3 objectColor;
+uniform Material material;
 
 void main()
 {
-	// BASIC LIGHTING PRACTICE 4
-	FragColor = vec4(LightingColor * objectColor, 1.0);
+	// ambient light
+    vec3 ambient = lightColor * material.ambient;
+
+    // diffuse reflection 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = lightColor * (diff * material.diffuse);
+
+    // specular reflection
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = lightColor * (spec * material.specular);  
+
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
 }
