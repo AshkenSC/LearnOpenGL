b0800c96b6be5847ed169cfbfe819a964230c04f
AshkenSC <393940378@qq.com>
2020-01-21 23:27:57 +0800

    Materials: Fragment shader has been updated: Set light intensity in different components.

--- colors.fs
+++ colors.fs
@@ -17,19 +17,19 @@ uniform Material material;
 void main()
 {
 	// ambient light
-    vec3 ambient = lightColor * material.ambient;
+	vec3 ambient  = vec3(1.0) * material.ambient;
 
     // diffuse reflection 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = lightColor * (diff * material.diffuse);
+	vec3 diffuse  = vec3(1.0) * (diff * material.diffuse);
 
     // specular reflection
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = lightColor * (spec * material.specular);  
+	vec3 specular = vec3(1.0) * (spec * material.specular);
 
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
